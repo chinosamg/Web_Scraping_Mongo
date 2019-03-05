af2ea65a9865ba3f9b74d148f79527b2542d61e7
Samuel Guzman <[chinosamg@gmail.com]>
2019-03-04 21:06:13 -0700

    final

--- app.py
+++ app.py
@@ -1,38 +1,29 @@
-# import necessary libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# create instance of Flask app
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 
-# Use flask_pymongo to set up mongo connection
 mongod = PyMongo(app)
 
 
-# create route that renders index.html template and finds documents from mongod
 @app.route("/")
 def home():
 
-    # Find data
     try:
         mars = list(mongod.db.collection.find())[-1]
     except:
         mars = {}
 
-    # return template and data
     return render_template("index.html", mars=mars)
 
 
-# Route that will trigger scrape functions
 @app.route("/scrape")
 def scrape():
 
-    # Run scraped functions
     mars = scrape_mars.scrape()
 
-    # Store results into a dictionary
     mars_dict = {
         "news_title": mars["news_title"],
         "news_p": mars["news_p"],
@@ -42,10 +33,10 @@ def scrape():
         "hemisphere_image_urls": mars["hemisphere_image_urls"]
     }
 
-    # Insert mars_dict into database
+    
     mongod.db.collection.insert_one(mars_dict)
 
-    # Redirect back to home page
+    
     return redirect("http://localhost:5000/", code=302)
 
 
